1eeb55e0f18e651a1dfbfd00f02d035c7166ffa3
Christian Unger <christian@sebs-nest.org>
2026-08-28 16:43:57 +1200

    Added monitor settings

--- hypr-user.lua
+++ hypr-user.lua
@@ -3,3 +3,26 @@ hl.on("hyprland.start", function()
 		"systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP && systemctl --user start hyprland-session.target"
 	)
 end)
+
+hl.monitor({
+	output = "DP-2",
+	mode = "2560x1440@240",
+	position = "1920x0",
+	scale = "1",
+})
+hl.monitor({
+	output = "HDMI-A-2",
+	mode = "1920x1080@60",
+	position = "0x180",
+	scale = "1",
+})
+hl.monitor({
+	output = "HDMI-A-1",
+	mode = "1920x1080@60",
+	position = "4480x180",
+	scale = "1",
+})
+
+hl.workspace_rule({ workspace = "1", monitor = "DP-2" })
+hl.workspace_rule({ workspace = "2", monitor = "HDMI-A-2" })
+hl.workspace_rule({ workspace = "3", monitor = "HDMI-A-1" })
